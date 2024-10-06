be7681dd1d0358ede841300668a3ad8f66c36119
Eddy602 <edwardodjidjaaa@gmail.com>
2024-10-06 15:54:20 +0300

    coditional statement

---
+++ main.py
@@ -0,0 +1,51 @@
+# age= int(input("enter your age"))
+# if age>18:
+#     print("allowed") 
+# else:
+#     print("not allowed")
+
+    #activity 2
+# number= int(input("enter a number"))
+# if number>0:
+#     print("positive")  
+# else:
+#     print("negetive")  
+     #activity 3
+#number= int(input("enter a number"))
+# if number%==2
+#     print("even")  
+# else:
+#     print("odd")  
+      #activity 4
+# height= int(input("enter height in cm"))
+# weight= int(input("enter weight in kg"))
+# bmi=weight/(height/100)**2
+# print(bmi)
+# if bmi<18.4:
+#     print("you are underweight")
+# elif bmi<24.9:
+#     print("you are healthy") 
+# elif bmi<29.9:
+#     print("you are overweight")  
+# elif bmi<34.9:
+#     print("sevearly overweight") 
+# elif bmi<39.9:
+#     print("obese")  
+# else:
+#     print("sevearly obese")              
+      #activity 5
+# number= int(input("enter number"))
+# if number>50:
+#      print("number is greater than 50")
+#      if number%2==0:
+#           print("even number")
+#      else:
+#           print("number is odd") 
+# else:
+#      print("number is less than 50")         
+       # activity 6
+import datetime
+currenttime=datetime.datetime.now()
+print(currenttime)
+import calendar
+print(calendar.calendar(2024))
